ea46fe2fba9bda8d28a48fd70241152d447a8ee9
lucashamacher <lucashamacher@arizona.edu>
2025-11-26 13:23:23 -0700

    Create CharData.lua

---
+++ Roblox/Modules/CharData.lua
@@ -0,0 +1,27 @@
+local Itadori = {
+	
+	Id = "Itadori1",
+	DisplayName = "Itadori Yuji [Vessel]",
+	Class = "Blitz",
+	
+	BaseStats = {
+		Health = 225,
+		Shield = 250
+	},
+	
+	Abilities = {
+		a = "Slaughter Demon Throw",
+		b = "Divergent Fist Dash",
+		c = "Unnatural Athleticism",
+		
+		special = "Strong Push",
+		ultimate = "Lose Control"
+	},
+	
+	Description = "A student chosen by the world of sorcerers and curses. A close-ranged fighter, with uncanny phyisical ability.",
+	PortraitID = nil, --doesnt exist yet :p
+	SprintAniID = "default"
+	
+}
+
+return Itadori
